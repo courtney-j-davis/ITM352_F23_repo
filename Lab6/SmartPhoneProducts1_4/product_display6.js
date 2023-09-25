237ff027c560de98e4e7fe0c7a3aa17b6bbd405c
courtney-j-davis <cvalley@hawaii.edu>
2023-09-24 14:20:14 -1000

    LAB six question 1

--- Lab6/SmartPhoneProducts1_4/product_display6.js
+++ Lab6/SmartPhoneProducts1_4/product_display6.js
@@ -1,76 +1,79 @@
+//define store name and push it to the DOM in the top title
+const store_name="Courtney";
+top_title.innerHTML=(store_name + "'s Used Smart Phone Store");
 
-        //define all my variables for the document
-        const store_name="Courtney";
-        let first_name= "Courtney";
-        let last_name= "Davis";
-        //making date and time variables to write in footer table
-        let now = new Date ();
-        let year = now.getFullYear();
-        let hours = now.getHours();
-        let minutes = now.getMinutes();
-        //add leading zeros to minutes 
-        minutes = (minutes<10)? "0"+minutes : minutes;
-        //convert hours to 12 hour format and determine AM/PM
-        let ampm = (hours >= 12)? "PM" : "AM";
-        hours= (hours % 12==0)? 12 : hours % 12;
-        // create a string to display the time in the HTML doc
-        let currentTime = hours + ":" + minutes + ampm;
+//send store name info to the footer title
+bottom_title.innerHTML=("Your one stop shop for used phones - "+store_name+"'s");            
 
+//initialize hits and spins and send to the DOM
+let hits= 0;
+let spins=0;
+//let wins;
+let over_half=false;
+hits_span.innerHTML = hits; 
+spins_span.innerHTML = spins;
 
-    document.write ("<strong>"+first_name+ "<strong/>"+" "+ last_name+"'s" +" " +"Used Smart Phone Store");
-        top_title.innerHTML=(store_name+"'s Used Smart Phone Store"); 
-        
-    function changeClassName (element){
-        element.className='item rotate';
-        spin=spins+1
-        spins_span.innerHTML= spins;
-        hit_spin_span.innerHTML=Number(hits/spins).toFixed(2)
-        if (spins<(2*hits)) {
-            wins=true;
-            over_half=true;
+//this should be two functions or more but... it changes the item name to rotate for the images and updates the spin count and calculates hits/spins ratio, sending both to the DOM
+function changeClassName(element) {
+    element.className=='item'
+    element.className = 'item rotate';
+    spins=spins+1;
+    if(spins<2*hits&&hits<spins){
+        //wins=true;
+        over_half=true;
+    } else {
+       // wins=false;
     }
-    else {
-        wins=false;
-    }
-        //win_span.innerHTML=wins;
-        win_span.innerHTML=over_half;
-    }
+    //win_span.innerHTML=wins;
+    win_span.innerHTML=over_half;
+    spins_span.innerHTML = spins; 
+    hit_spin_span.innerHTML=(hits/spins).toFixed(2);
+}
 
-    function resetClassName(element) {
-        element.className='item';
+//this should be two functions or more but... it changes the item name by removing the rotate for the images and updates the hits count and calculates hits/spins ratio, sending both to the DOM
+function resetClassName(element) {
+    if(element.className == 'item rotate'){
+        element.changeClassName='item'
+        hits=hits+=2;
+    };
 
-    function changeClassName (element){
-            element.className='item rotate';
+    if(spins<2*hits&&hits<spins){
+        //wins=true;
+        over_half=true;
+    } else {
+        //wins=false;
+    }
+    //win_span.innerHTML=wins;
+    win_span.innerHTML=over_half;
+    hits_span.innerHTML = hits; 
+    hit_spin_span.innerHTML=(hits_span.innerHTML/spins_span.innerHTML).toFixed(2);
+}
+// -- Winning progress depends on hits/spins
+//let hits_spins_ratio = hits/spins;
+//let progess;
+/*if ( hits_spins_ratio > 0 ) {
+    progress = 'On your way!';
+    if ( hits_spins_ratio >= 0.25 ) {
+        progress = 'Almost there!';
+        if ( hits_spins_ratio >= 0.5 ) {
+            if( hits < spins) { 
+                progress = 'You win!';
+            }
         }
-    function resetClassName(element) {
-            element.className='item';
-        }
-        let hits= 0;
-        let spins=0;
-        let wins;
-        let over_half=false;
-        hits_span.innerHTML=hits;
-            spins_span.innerHTML=spins;
-            
     }
-    let hits= 0;
-    let spins=0;
-    hits_span.innerHTML=hits;
-        spins_span.innerHTML=spins;
+}
+else {
+    progress = 'Get going!' ;
+}
 
-//Lab 6 question 1a) on a mouse over the spins increase on a click hits increase. 
-        
- 
- 
-        document.write("Your One Stop Phone Shop - "+store_name+"'s");
-    
-    
-                document.write ("Copyright @ "+first_name+ " "+last_name);
-        
-
-        
-                document.write (year)
-        
-            
-                document.write (currentTime);
-        
+if(hits_spins_ration>0) {
+    progress='on your way!';
+} else if (hits_spins_ratio >=0.5){else
+You can't use the nested if statements needs to be reevaluated. try goin backwards start with .5
+if (hits<spins){progress='YOu win!';}
+if ( hits_spins_ratio >= 0.25 ) {
+        progress = 'Almost there!';
+        else if (hits_spins_ratio>0)
+        progress = 'On your way!';
+}
+*/

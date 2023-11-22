cc9b91c383775cdfe4250c805895308654111076
courtney-j-davis <cvalley@hawaii.edu>
2023-11-22 10:29:26 -1000

    lab14

---
+++ Lab1/Lab14/L14b1.js
@@ -0,0 +1,14 @@
+const fs=require('fs');
+
+let filename = __dirname+ '/user_data.json';
+
+if (fs.existsSync(filename)){
+    let data = fs.readFileSync(filename, 'utf-8');
+        //encodes and read the file synchro. 
+    let user_reg_data = JSON.parse(data);
+    //this line, after the file contents is read this code uses the JASON parse functionality to parse the data into a js object which is necessary be the 'readFileSynce' returns as a string, we want JSON formatted data to use not a string 
+
+    console.log(user_reg_data);
+}else {
+    console.log(`The file name ${filename}does not exist.`);
+}
